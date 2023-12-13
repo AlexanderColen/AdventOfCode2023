c2c8c30e0a244528287bbb0ffb65cc3f60316f2f
Alexander Colen <alexanderc@live.nl>
2023-12-13 15:50:45 -0500

    Small cleanup for Day 2 - Puzzle 1

--- Day02/Puzzle1.cs
+++ Day02/Puzzle1.cs
@@ -10,18 +10,18 @@ namespace AdventOfCode2023.Day02
         public override void RunPuzzle()
         {
             var input = ReadPuzzleInputAsStringList();
-            var totalInvalidIDSum = 0;
+            var totalValidIDSum = Enumerable.Range(1, 100).Sum();
             // Regular expression matching invalid 2 digit draws.
             var regExp = new Regex("(([1][3-9]|[2-9][0-9]) red)|(([1][4-9]|[2-9][0-9]) green)|(([1][5-9]|[2-9][0-9]) blue)");
 
             for (var i = 0; i < input.Count; i++) {
                 if (regExp.IsMatch(input[i]))
                 {
-                    totalInvalidIDSum += i + 1;
+                    totalValidIDSum -= i + 1;
                 }
             }
 
-            Console.WriteLine($"Sum of valid game IDs: {Enumerable.Range(1, 100).Sum() - totalInvalidIDSum}");
+            Console.WriteLine($"Sum of valid game IDs: {totalValidIDSum}");
         }
     }
 }
