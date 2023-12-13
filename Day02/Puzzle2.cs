1eee0abe00d0ff487ff87d67190a685fb93c0e76
Alexander Colen <alexanderc@live.nl>
2023-12-13 16:10:22 -0500

    Day 2 - Puzzle 2

---
+++ Day02/Puzzle2.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode2023.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Day02
+{
+    class Puzzle2 : Puzzle
+    {
+        public override void RunPuzzle()
+        {
+            var games = ReadPuzzleInputAsStringList();
+            var totalPowerSum = 0;
+            var colors = new List<string>() { "red", "green", "blue" };
+
+            foreach (var game in games)
+            {
+                var fewestCubes = new List<int>();
+
+                foreach (var color in colors)
+                {
+                    var fewest = 0;
+
+                    foreach (var match in new Regex($"\\d+ {color}").Matches(game))
+                    {
+                        var current = int.Parse(match.ToString().Split(' ')[0]);
+                        if (fewest < current)
+                        {
+                            fewest = current;
+                        }
+                    }
+
+                    fewestCubes.Add(fewest);
+                }
+
+                totalPowerSum += fewestCubes.Aggregate((a, x) => a * x);
+            }
+
+            Console.WriteLine($"Sum of total set powers: {totalPowerSum}");
+        }
+    }
+}
